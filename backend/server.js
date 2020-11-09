83d5a3878b2e5327ab2dd8ff0adff362011e01b0
Francisco Castillo <fjcasti1@asu.edu>
2020-11-09 12:16:30 -0700

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import 'colors';
 import connectDB from './config/db.js';
@@ -11,12 +12,22 @@ connectDB();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 // Products Routes
 app.use('/api/products', productRoutes);
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')),
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 // 404 Handling
 app.use(notFound);
