c37be8680797b2b848b34b8da0e84b04ae9be263
Francisco Castillo <fjcasti1@asu.edu>
2020-11-09 18:22:16 -0700

    User Registration & Password Encryption

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import protect from '../middleware/authMiddleware.js';
-import { authUser, getUserProfile } from '../controllers/userController.js';
+import {
+  registerUser,
+  authUser,
+  getUserProfile,
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
+// Register user
+router.post('/', registerUser);
+// Log in user
 router.post('/login', authUser);
+// Get logged in user
 router.get('/profile', protect, getUserProfile);
 
 export default router;
