72602c8fb40e9cb04baac68f4e7e9aec8cfc8b2e
Francisco Castillo <fjcasti1@asu.edu>
2020-11-10 15:30:53 -0700

    Order Controller & Route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import 'colors';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -20,6 +21,8 @@ app.use(express.json());
 app.use('/api/products', productRoutes);
 // Users Routes
 app.use('/api/users', userRoutes);
+// Order Routes
+app.use('/api/orders', orderRoutes);
 
 const __dirname = path.resolve();
 
