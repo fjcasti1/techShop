b825a17677dd6d48b54ffd8530fd74fe24a442ae
Francisco Castillo <fjcasti1@asu.edu>
2020-11-27 15:01:07 -0700

    Create Review Endpoint

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,13 +6,15 @@ import {
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 } from '../controllers/productController.js';
 
 const router = express.Router();
 
 router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
 router.route('/').post(protect, protectAdmin, createProduct);
+router.route('/:id').get(getProductById);
+router.route('/:id/reviews').post(protect, createProductReview);
 router.route('/:id').put(protect, protectAdmin, updateProduct);
 router.route('/:id').delete(protect, protectAdmin, deleteProduct);
 
