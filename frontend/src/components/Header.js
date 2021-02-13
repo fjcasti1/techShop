2c6a331eea02283a6c6e7ace625aeba0e9a38a57
Francisco Castillo <fjcasti1@asu.edu>
2021-02-12 17:14:31 -0700

    Change store name

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <Navbar.Brand as={Link} to='/'>
-            eCommerce
+            techShop
           </Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
