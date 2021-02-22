4ca270f62ed490696e4ccf9307a489da7fa05488
Francisco Castillo <fjcasti1@asu.edu>
2021-02-22 14:06:07 -0700

    Rename protect for isAuth and isAdmin

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { protect, protectAdmin } from '../middleware/authMiddleware.js';
+import { isAuth, isAdmin } from '../middleware/authMiddleware.js';
 import {
   getProducts,
   getProductById,
@@ -14,10 +14,10 @@ const router = express.Router();
 
 router.route('/').get(getProducts);
 router.get('/top', getTopProducts);
-router.route('/').post(protect, protectAdmin, createProduct);
+router.route('/').post(isAuth, isAdmin, createProduct);
 router.route('/:id').get(getProductById);
-router.route('/:id/reviews').post(protect, createProductReview);
-router.route('/:id').put(protect, protectAdmin, updateProduct);
-router.route('/:id').delete(protect, protectAdmin, deleteProduct);
+router.route('/:id/reviews').post(isAuth, createProductReview);
+router.route('/:id').put(isAuth, isAdmin, updateProduct);
+router.route('/:id').delete(isAuth, isAdmin, deleteProduct);
 
 export default router;
