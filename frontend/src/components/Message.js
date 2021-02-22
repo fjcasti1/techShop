fa43df1ab6d7dca88b46e924ef9243747b97fec7
Francisco Castillo <fjcasti1@asu.edu>
2021-02-22 14:02:03 -0700

    Add dismissible option

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Alert } from 'react-bootstrap';
 
-const Message = ({ variant, children }) => {
-  return <Alert variant={variant}>{children}</Alert>;
+const Message = ({ variant, dismissible, children }) => {
+  const [show, setShow] = useState(true);
+
+  if (show) {
+    return (
+      <Alert
+        variant={variant}
+        className='rounded'
+        onClose={() => setShow(false)}
+        dismissible={dismissible}
+      >
+        {children}
+      </Alert>
+    );
+  } else {
+    return <div></div>;
+  }
 };
 
 Message.defaultProps = {
